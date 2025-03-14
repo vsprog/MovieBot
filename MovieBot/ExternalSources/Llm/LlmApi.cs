96464a2f3de3bd52c057405b36aa363ccd6e6cad
vsprog <chmolekula@gmail.com>
2025-03-14 18:24:04 +0700

    debug

--- MovieBot/ExternalSources/Llm/LlmApi.cs
+++ MovieBot/ExternalSources/Llm/LlmApi.cs
@@ -20,21 +20,27 @@ public class LlmApi
 
     public async Task<IEnumerable<string>> GetAnswer(string message, CancellationToken cancellationToken)
     {
-        _previousMessages.Add(new LlmMessage(LlmRole.User, message));
+        _previousMessages.Add(new LlmMessage("user", message));
         
-        var json = JsonConvert.SerializeObject(new LlmRequest
+        // var json = JsonConvert.SerializeObject(new LlmRequest
+        // {
+        //     Model = _llmConfig.Model,
+        //     Temperature = 1,
+        //     Messages = _previousMessages
+        // });
+        
+        //var response = await _client.PostAsync(string.Empty, new StringContent(json), cancellationToken);
+        var response = await _client.PostAsJsonAsync(string.Empty, new LlmRequest
         {
             Model = _llmConfig.Model,
             Temperature = 1,
             Messages = _previousMessages
-        });
-        
-        var response = await _client.PostAsync(string.Empty, new StringContent(json), cancellationToken);
+        }, cancellationToken);
         
         if (response.StatusCode != HttpStatusCode.OK)
         {
             //Constants.DefaultLlmAnswer
-            return new[]{ json, response.StatusCode.ToString(), response.RequestMessage?.ToString() ?? "" };
+            return new[]{  response.StatusCode.ToString(), response.RequestMessage?.ToString() ?? "" };
         }
         
         var content = await response.Content.ReadAsStringAsync(cancellationToken);
@@ -42,7 +48,7 @@ public class LlmApi
 
         var answers =  data!.Choices.Select(c => c.Message.Content).ToList();
 
-        _previousMessages.AddRange(answers.Select(a => new LlmMessage(LlmRole.Assistant, a)));
+        _previousMessages.AddRange(answers.Select(a => new LlmMessage("assistant", a)));
         
         return answers;
     }
