772c3d028ff8d093bc9c0ab8be1f2248edbf42ee
vsprog <chmolekula@gmail.com>
2025-03-14 17:10:17 +0700

    check single response

--- MovieBot/ExternalSources/Llm/LlmApi.cs
+++ MovieBot/ExternalSources/Llm/LlmApi.cs
@@ -10,7 +10,7 @@ public class LlmApi
 {
     private readonly HttpClient _client;
     private readonly LlmConfiguration _llmConfig;
-    private readonly List<LlmMessage> _previousMessages = new();
+    //private readonly List<LlmMessage> _previousMessages = new();
     
     public LlmApi(HttpClient client, IOptions<LlmConfiguration> llmOptions)
     {
@@ -20,12 +20,12 @@ public class LlmApi
 
     public async Task<IEnumerable<string>> GetAnswer(string message, CancellationToken cancellationToken)
     {
-        _previousMessages.Add(new LlmMessage(LlmRole.User, message));
+        //_previousMessages.Add(new LlmMessage(LlmRole.User, message));
         
         var response = await _client.PostAsJsonAsync(string.Empty, new LlmRequest
         {
             Model = _llmConfig.Model,
-            Messages = _previousMessages
+            Messages = new List<LlmMessage>{new LlmMessage(LlmRole.User, message)}
         }, cancellationToken);
         
         if (response.StatusCode != HttpStatusCode.OK)
@@ -38,7 +38,7 @@ public class LlmApi
 
         var answers =  data!.Choices.Select(c => c.Message.Content).ToList();
 
-        _previousMessages.AddRange(answers.Select(a => new LlmMessage(LlmRole.Assistant, a)));
+        //_previousMessages.AddRange(answers.Select(a => new LlmMessage(LlmRole.Assistant, a)));
         
         return answers;
     }
