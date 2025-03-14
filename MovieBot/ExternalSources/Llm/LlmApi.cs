64b9ff84f078eacbad8e111e93f1ba736a139994
vsprog <chmolekula@gmail.com>
2025-03-14 17:07:19 +0700

    remove stack

--- MovieBot/ExternalSources/Llm/LlmApi.cs
+++ MovieBot/ExternalSources/Llm/LlmApi.cs
@@ -20,7 +20,7 @@ public class LlmApi
 
     public async Task<IEnumerable<string>> GetAnswer(string message, CancellationToken cancellationToken)
     {
-        _previousMessages.Insert(0, new LlmMessage(LlmRole.User, message));
+        _previousMessages.Add(new LlmMessage(LlmRole.User, message));
         
         var response = await _client.PostAsJsonAsync(string.Empty, new LlmRequest
         {
@@ -38,10 +38,7 @@ public class LlmApi
 
         var answers =  data!.Choices.Select(c => c.Message.Content).ToList();
 
-        foreach (var ans in answers.Select(a => new LlmMessage(LlmRole.Assistant, a)))
-        {
-            _previousMessages.Insert(0, ans);
-        } 
+        _previousMessages.AddRange(answers.Select(a => new LlmMessage(LlmRole.Assistant, a)));
         
         return answers;
     }
